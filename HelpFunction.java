1bc3c33a720e9ec7fbcf6adc1960b98be257e81e
TortCode <44101297+TortCode@users.noreply.github.com>
2019-09-04 18:09:26 -0400

    Update HelpFunction.java

--- HelpFunction.java
+++ HelpFunction.java
@@ -1,4 +1,4 @@
-public void isPathClear(Square dest) { //path must be vertical,horizontal,diagonal
+public void isPathClear(Square dest, boolean capture) { //path must be vertical,horizontal,diagonal
   if(dest == location) return true;
   int r = location.getRow(), c = location.getCol();
   int endr = dest.getRow(), endc = dest.getCol();
@@ -10,5 +10,22 @@ public void isPathClear(Square dest) { //path must be vertical,horizontal,diagon
       r += dr;
       c += dc;
   }
+  if (dest.getPiece() != null)
+    return capture;
   return true;
+}
+//pawn isMoveLegal
+public boolean isMoveLegal(Square dest) {
+  int dir = (getTeam())? -1 : 1; //move down for black, up for white
+  int dr = rowDiff(dest), dc = colDiff(dest);
+  
+  if (firstMove && dr == dir*2 && dc == 0)
+    return isPathClear(dest,false);
+  if (dr == -1 && dc == 0) {
+    return dest.getPiece() == n
+  }
+  
+  
+  
+  
 }
