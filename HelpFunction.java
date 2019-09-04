34c6c26460de1ec4237eb1b41edb766312e337c7
TortCode <44101297+TortCode@users.noreply.github.com>
2019-09-04 19:10:04 -0400

    Update HelpFunction.java

--- HelpFunction.java
+++ HelpFunction.java
@@ -1,4 +1,4 @@
-public void isPathClear(Square dest, boolean capture) { //path must be vertical,horizontal,diagonal
+public boolean isPathClear(Square dest, boolean captureAllowed) { //path must be vertical,horizontal,diagonal
   if(dest == location) return true;
   int r = location.getRow(), c = location.getCol();
   int endr = dest.getRow(), endc = dest.getCol();
@@ -7,11 +7,11 @@ public void isPathClear(Square dest, boolean capture) { //path must be vertical,
   while (r != destr || c != destc) {
     if (squares[r][c].getPiece() != null)
       return false;
-      r += dr;
-      c += dc;
+    r += dr;
+    c += dc;
   }
   if (dest.getPiece() != null)
-    return capture;
+    return captureAllowed;
   return true;
 }
 //pawn isMoveLegal
@@ -19,13 +19,13 @@ public boolean isMoveLegal(Square dest) {
   int dir = (getTeam())? -1 : 1; //move down for black, up for white
   int dr = rowDiff(dest), dc = colDiff(dest);
   
+  if (dr == dir*1 && dc == 0) {
+    return dest.getPiece() == null;
+  }
+  if (dr == dir*1 && Math.abs(dc)==1)
+    return dest.getPiece() != null;
   if (firstMove && dr == dir*2 && dc == 0)
     return isPathClear(dest,false);
-  if (dr == -1 && dc == 0) {
-    return dest.getPiece() == n
-  }
   
-  
-  
-  
+  return false;
 }
