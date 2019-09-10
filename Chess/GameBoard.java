2f4fba65bfae0b642a126271cf018d88d323897d
TortCode <44101297+TortCode@users.noreply.github.com>
2019-09-10 13:35:55 -0400

    Update GameBoard.java

--- Chess/GameBoard.java
+++ Chess/GameBoard.java
@@ -3,8 +3,7 @@ import javax.swing.*;
 
 public class GameBoard extends JFrame {
 	public static final int ROWS = 8, COLS = 8;
-	private static final String[] promoteOptions = {"Queen", "Knight", "Rook",
-			"Bishop"};
+	private static final String[] promoteOptions = { "Queen", "Knight", "Rook", "Bishop" };
 	private static final int QUEEN = 0, KNIGHT = 1, ROOK = 2, BISHOP = 3;
 	private static boolean turn = false;
 	static Square[][] squares = new Square[ROWS][COLS];
@@ -55,11 +54,12 @@ public class GameBoard extends JFrame {
 		bKing = new King(true, squares[0][4]);
 		wKing = new King(false, squares[7][4]);
 		// some finishing touches
-		this.setSize(1000, 1000);
+		this.setSize(900, 900);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
+
 	// click handler stores origin and destination
 	// executes legal moves after dest selection
 	public void clicked(Square sq) {
@@ -71,11 +71,8 @@ public class GameBoard extends JFrame {
 				// highlight possible moves
 				for (int r = 0; r < ROWS; r++)
 					for (int c = 0; c < COLS; c++)
-						if (cp.isMoveLegal(squares[r][c])) {
+						if (cp.isMoveLegal(squares[r][c], true))
 							squares[r][c].setHighlight(true);
-							ENPASSANT = false;
-							CASTLING = false;
-						}
 			}
 		} else if (src != null && dest == null) {
 			// reset highlight
@@ -85,60 +82,53 @@ public class GameBoard extends JFrame {
 					squares[r][c].setHighlight(false);
 			dest = sq;
 			ChessPiece cp = src.getPiece();
-			if (cp.isMoveLegal(dest)) {
+			if (cp.isMoveLegal(dest, false)) {
 				passable = null;
 				cp.move(dest);
 				// is pawn promoted
-				if (cp instanceof Pawn
-						&& dest.getRow() == (turn ? GameBoard.ROWS - 1 : 0))
+				if (cp instanceof Pawn && dest.getRow() == (turn ? GameBoard.ROWS - 1 : 0))
 					promotePawn((Pawn) cp);
 				// is King captured
 				if (dump instanceof King) {
-					JOptionPane.showMessageDialog(this,
-							(turn ? "Black" : "White") + " wins!", "Victory",
+					JOptionPane.showMessageDialog(this, (turn ? "Black" : "White") + " wins!", "Victory",
 							JOptionPane.INFORMATION_MESSAGE);
 					System.exit(0);
 				}
 				// is King checked
 				if (bKing.underAttack())
-					JOptionPane.showMessageDialog(this, "Black checked",
-							"Check", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(this, "Black checked", "Check", JOptionPane.INFORMATION_MESSAGE);
 				if (wKing.underAttack())
-					JOptionPane.showMessageDialog(this, "White checked",
-							"Check", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(this, "White checked", "Check", JOptionPane.INFORMATION_MESSAGE);
 				turn = !turn;
-
 			}
 			src = null;
 			dest = null;
 		}
 	}
+
 	private void promotePawn(Pawn p) {
-		int newcp = JOptionPane.showOptionDialog(this, "Choose replacement",
-				"Pawn Promotion", JOptionPane.DEFAULT_OPTION,
-				JOptionPane.INFORMATION_MESSAGE, null, promoteOptions,
-				promoteOptions[0]);
+		int newcp = JOptionPane.showOptionDialog(this, "Choose replacement", "Pawn Promotion",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, promoteOptions, promoteOptions[0]);
 		Square sq = p.location;
 		p.location = null;
 		boolean tm = p.getTeam();
 		ChessPiece cp;
 		switch (newcp) {
-			case QUEEN :
-				cp = new Queen(tm, sq);
-				break;
-			case KNIGHT :
-				cp = new Knight(tm, sq);
-				break;
-			case ROOK :
-				cp = new Rook(tm, sq);
-				break;
-			case BISHOP :
-				cp = new Bishop(tm, sq);
-				break;
-			default :
-				cp = new Queen(tm, sq);
-				JOptionPane.showMessageDialog(this, "Default to Queen",
-						"Improper Input Exit", JOptionPane.ERROR_MESSAGE);
+		case QUEEN:
+			cp = new Queen(tm, sq);
+			break;
+		case KNIGHT:
+			cp = new Knight(tm, sq);
+			break;
+		case ROOK:
+			cp = new Rook(tm, sq);
+			break;
+		case BISHOP:
+			cp = new Bishop(tm, sq);
+			break;
+		default:
+			cp = new Queen(tm, sq);
+			JOptionPane.showMessageDialog(this, "Default to Queen", "Improper Input Exit", JOptionPane.ERROR_MESSAGE);
 		}
 		cp.firstMove = false;
 	}
