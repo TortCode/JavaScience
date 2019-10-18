3853d71aed916e8cadbe2ba55327afa2ca59f2af
TortCode <44101297+TortCode@users.noreply.github.com>
2019-10-18 11:40:28 -0400

    Update BodySegment.java

--- Snake/BodySegment.java
+++ Snake/BodySegment.java
@@ -2,31 +2,29 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class BodySegment {
-	//this BodySegment will act like a NODE
-	//  he will point to the next BodySegment in the Snake
-	//  You will need a variable for this
-	
-	public static final int SIZE=15;
+	// this BodySegment will act like a NODE
+	// he will point to the next BodySegment in the Snake
+	// You will need a variable for this
+
+	public static final int SIZE = 15;
 	public Color hue = Color.green;
 	public static int nextID = 0;
 	public int myID;
 	private int xPos, yPos;
-	
-	public BodySegment(int x, int y){
+
+	public BodySegment(int x, int y) {
 		xPos = x;
 		yPos = y;
-		
+
 		myID = nextID++;
 	}
-	
-	//~~~~~~~~~~~~~~~~accessors and mutators~~~~~~~~~~~~~~~~~~~~~~
-	
+
+	// ~~~~~~~~~~~~~~~~accessors and mutators~~~~~~~~~~~~~~~~~~~~~~
+
 	public boolean isTouching(BodySegment sp) {
-		int dx = sp.xPos - this.xPos;
-		int dy = sp.yPos - this.yPos;
-		return dx*dx + dy*dy < SIZE*SIZE;
+		return sp.xPos == this.xPos && sp.yPos == this.yPos;
 	}
-	
+
 	public int getXPos() {
 		return xPos;
 	}
@@ -35,12 +33,12 @@ public class BodySegment {
 		return yPos;
 	}
 
-	//each BodySegment knows how to draw himself :)
-	public void draw(Graphics g){
+	// each BodySegment knows how to draw himself :)
+	public void draw(Graphics g) {
 		g.setColor(hue);
 		g.fillOval(xPos, yPos, SIZE, SIZE);
-		//just for testing purposes, you can take this out later
+		// just for testing purposes, you can take this out later
 		g.setColor(Color.BLACK);
-		g.drawString(""+myID, xPos+SIZE/4, yPos+SIZE);
+		g.drawString("" + myID, xPos + SIZE / 4, yPos + SIZE);
 	}
 }
